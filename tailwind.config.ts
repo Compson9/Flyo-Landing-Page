b813e183f2a81369d261aabc72152ac1f9d0e4d6
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-01-15 21:56:34 +0000

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      "sm": "425x",
+      "lg": "1440px"
+
+    },
+    colors: {
+      "cyan": "#65E2D9",
+      "darkgreen": "#1C2431",
+      "white": "#ffff",
+      "lightblue": "#202A3C",
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
